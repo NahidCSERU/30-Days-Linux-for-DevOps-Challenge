87f053b903e1af4e9d6b9148178662699d4af107
NahidCSERU <nahid.hasancseru@gmail.com>
2025-08-19 17:14:21 +0600

    feat(less): day-03 less_example.sh file add to perform  bash  script

---
+++ day03-text-file-handling/scripts/less_examples.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Day-02: Examples of less command
+# Author: <Nahid Hasan>
+# Date: $(19/08/2025)
+echo "===== Using less with sample.txt ====="
+echo "Tip: Use 'q' to quit less viewer"
+
+less ../sample.txt

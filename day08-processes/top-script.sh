e72e282e8718067f19ca6f7818c24cb55741b646
NahidCSERU <nahid.hasancseru@gmail.com>
2025-08-25 01:01:05 +0600

    feat(bash): day 08 top command usages

---
+++ day08-processes/top-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Day 8 - Processes with top command
+# Author: Nahid Hasan
+# Date: (24.08.2025)
+
+echo "🔹 Launching top command (Press 'q' to quit)"
+top -n 1 -b | head -15
+
+echo ""
+echo "✅ top command demo finished!"

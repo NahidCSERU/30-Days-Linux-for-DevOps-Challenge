6c3f981751ee7fbebf468fd7d234f1aaefab2fa0
NahidCSERU <nahid.hasancseru@gmail.com>
2025-09-07 23:11:31 +0600

    user input + system info

---
+++ day22-bash-basics/scripts/system_info.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Using echo with system variables
+
+echo "Logged in user: $USER"
+echo "Home directory: $HOME"
+echo "Current shell: $SHELL"
+echo "Today's date: $(date)"
---
+++ day22-bash-basics/scripts/user_input.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Taking user input and using variables
+
+echo "Enter your name:"
+read USERNAME
+
+echo "Welcome, $USERNAME!"

c0450ebca18871e8be682489b0ab582b8af92157
NahidCSERU <nahid.hasancseru@gmail.com>
2025-09-15 19:59:13 +0600

    feat(cloud provider and variables)

---
+++ day30-final-project/variables.tf
@@ -0,0 +1,11 @@
+variable "project_name" {
+  default = "day30-final-project"
+}
+
+variable "db_username" {
+  default = "admin"
+}
+
+variable "db_password" {
+  default = "SuperSecurePass123!"
+}

a7feff9391c2602a4d835f98c65dd0a42695ad0a
NahidCSERU <nahid.hasancseru@gmail.com>
2025-09-15 20:01:14 +0600

    feat(terraform): Security Groups create

--- day30-final-project/main.tf
+++ day30-final-project/main.tf
@@ -38,3 +38,50 @@ resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public_rt.id
 }
+#################################
+# Security Groups
+#################################
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.main.id
+  name   = "${var.project_name}-alb-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = aws_vpc.main.id
+  name   = "${var.project_name}-ec2-sg"
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
